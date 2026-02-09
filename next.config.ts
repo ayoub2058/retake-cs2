f40227b38469013d9fd62ed6458f966ad28509be
ayoub2058 <168081448+ayoubbezi@users.noreply.github.com>
2026-02-09 18:33:46 +0100

    last commit

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,6 @@ import type { NextConfig } from "next";
 const allowBuildErrors = process.env.ALLOW_BUILD_ERRORS === "true";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: allowBuildErrors,
-  },
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
